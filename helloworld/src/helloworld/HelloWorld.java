6b32b6b87690db7bbcc6419c1e69a688580eec93
Stephany camily <stefycamly@gmail.com>
2023-03-14 12:00:30 -0300

    projeto Hello World

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int teste = 10;
+		
+		System.out.println("Hello World");
+		System.out.println("\nOlá Turma 62!");
+		System.out.println("variável teste: " + teste);
+
+	}
+
+}
