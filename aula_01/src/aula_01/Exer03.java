9e79752b371aded8e8893e94397b62fb2011d059
Stephany camily <stefycamly@gmail.com>
2023-03-15 22:59:32 -0300

    Exercícos feitos 1 a 4

--- aula_01/src/aula_01/Exer03.java
+++ aula_01/src/aula_01/Exer03.java
@@ -1,11 +1,38 @@
 package aula_01;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exer03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	Scanner leia = new Scanner(System.in);
+	DecimalFormat df = new DecimalFormat("###,##0.00");
+	
+	float salarioBruto, adicionalNoturno, desconto, salarioLiquido; 
+	int horaExtra;
+		
+	System.out.println("Digite o salário bruto:");
+	salarioBruto = leia.nextFloat();
+	System.out.println("Digite o adicional noturno:");
+	adicionalNoturno = leia.nextFloat();
+	System.out.println("Digite o numero de horas extras:");
+	horaExtra = leia.nextInt();
+	System.out.println("Digite o desconto:");
+	desconto = leia.nextFloat();
+	
+	salarioLiquido= salarioBruto + adicionalNoturno + (float) (horaExtra * 5) - desconto;
+   
+	System.out.println("O Salário Líquido é: "+ df.format(salarioLiquido));
+	
+	leia.close();
 
 		
 	}
 
-}
+	
+	
+	}
+
+
--- aula_01/src/aula_01/Exer04.java
+++ aula_01/src/aula_01/Exer04.java
@@ -1,9 +1,30 @@
 package aula_01;
 
-public class Exer04 {
+import java.util.Scanner;
 
+public class Exer04 {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	Scanner leia = new Scanner(System.in);
+	
+	float num1, num2, num3, num4, diferenca;
+	
+	System.out.println("Digite o primeiro numero:");
+	num1= leia.nextFloat();
+	System.out.println("Digite o segundo numero:");
+	num2= leia.nextFloat();
+	System.out.println("Digite o terceiro numero:");
+	num3= leia.nextFloat();
+	System.out.println("Digite o quarto numero:");
+	num4= leia.nextFloat();
+	
+	diferenca = (num1 * num2) - (num3 * num4);
+	
+	System.out.print("A diferença é:" + diferenca);
+	
+	leia.close();
+	
 
 	}
 
