7b34d719be15e4623f960c074f23696df6610e24
Stephany Camily <111828694+stefygrr@users.noreply.github.com>
2023-03-27 00:26:05 -0300

    Update ExerCores.java

--- aula_05/src/aula_05/ExerCores.java
+++ aula_05/src/aula_05/ExerCores.java
@@ -1,15 +1,30 @@
 package aula_05;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ExerCores {
 
 	public static void main(String[] args) {
-		
-		ArrayList<String> cores = new ArrayList<>();
+	
+		ArrayList<String> cores = new ArrayList();
 		Scanner leia = new Scanner(System.in);
 		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Digite uma cor: ");
+			cores.add(leia.next());
+		}
+		
+		System.out.println(cores);
+		System.out.println();
+		Collections.sort(cores);
+		System.out.println(cores);
+		
+
+	}
+
+}
 		
 		
 		
